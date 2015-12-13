0a07fb585e2e499b849ec6266339e846d15ddd1c
Samuel Flis <samuel.d.flis@gmail.com>
2015-12-13 11:13:18 +0100

    Added errors to the calibration.

--- task_b/analysis/calibration.py
+++ task_b/analysis/calibration.py
@@ -91,6 +91,8 @@ def lin_fun(x,k,m):
     return x*k+m
 #Fitting a line to the input voltage and channel peak
 popt20, pcov20 = curve_fit(lin_fun, pulseHeights20, peaks20[1] ,p0 = [10.0,0])
+
+
 popt50, pcov50 = curve_fit(lin_fun, pulseHeights50, peaks50[1] ,p0 = [10.0,0])
 popt100, pcov100 = curve_fit(lin_fun, pulseHeights100, peaks100[1] ,p0 = [10.0,0])
 plt.figure()
@@ -112,9 +114,11 @@ plt.xlabel("Input pulse height [mV]")
 plt.legend()
 
 #Fitting a line to the channel peak and input voltage (inverse of what was done above)
-popt20, pcov20 = curve_fit(lin_fun, np.array(peaks20[1]), np.array(pulseHeights20), p0 = [1/10.,0])
-popt50, pcov50 = curve_fit(lin_fun, np.array(peaks50[1]), np.array(pulseHeights50) ,p0 = [1/10.0,0])
-popt100, pcov100 = curve_fit(lin_fun, np.array(peaks100[1]), np.array(pulseHeights100) ,p0 = [1/10.0,0])
+pulseheight_er = 0.3
+popt20, pcov20 = curve_fit(lin_fun, np.array(peaks20[1]), np.array(pulseHeights20), p0 = [1/10.,0],sigma = np.ones(5)*pulseheight_er)
+popt50, pcov50 = curve_fit(lin_fun, np.array(peaks50[1]), np.array(pulseHeights50), p0 = [1/10.0,0],sigma = np.ones(5)*pulseheight_er)
+popt100, pcov100 = curve_fit(lin_fun, np.array(peaks100[1]), np.array(pulseHeights100), p0 = [1/10.0,0],sigma = np.ones(4)*pulseheight_er)
+
 plt.figure()
 x = np.linspace(0,512,100)
 plt.errorbar(peaks20[1],pulseHeights20,xerr=peaks20[2],color = 'blue',label = "Gain 20, mV/channel %f"%popt20[0],ls = 'None')
@@ -130,12 +134,99 @@ plt.plot(x,lin_fun(x,popt100[0],popt100[1]),color = 'green')
 plt.xlabel("Channel")
 plt.ylabel("Input pulse height [mV]")
 plt.legend()
+
+
+#Fitting the slopes and Intercepts to the gains for extrapolation and interpolation
+gains = np.array([20,50,100])
+k = np.array([popt20[0],popt50[0],popt100[0]])
+m = np.array([popt20[1],popt50[1],popt100[1]])
+
+#Errors taken from the fits
+k_err =  np.sqrt(np.array([pcov20[0][0],pcov50[0][0],pcov100[0][0]]))
+m_err =  np.sqrt(np.array([pcov20[1][1],pcov50[1][1],pcov100[1][1]]))
+
+def powe(x,n):
+    return n*x**(-1.0)
+def poweg(x,n,i):
+    return n*x**(i)
+def powegdi(x,n,i):
+    return n*np.log(x)*x**i
+def powegdn(x,n,i):
+        return x**i
+poptgain3, pcovgain3 =curve_fit(poweg, gains, k , sigma= k_err,p0 = [1.0,-1])
+
+
+plt.figure()
+plt.errorbar(gains,k,yerr=k_err,ls='None',marker='o')
+g = np.linspace(1,500,1000)
+plt.plot(g,poweg(g,poptgain3[0],poptgain3[1]),color='blue',
+        label='normalization = %f'%poptgain3[0]+r"$\pm$ "+"%f, index = %f"%(np.sqrt(pcovgain3[0][0]),poptgain3[1]) +r"$\pm$ "+"%f"%np.sqrt(pcovgain3[1][1]))
+
+plt.yscale('log')
+plt.xlabel("gain")
+plt.ylabel("mV/channel")
+plt.legend()
+
+
+
+plt.figure()
+plt.title("Intercepts")
+poptgainint, pcovgainint =curve_fit(poweg, gains, m , sigma = m_err, p0 = [1.0,-1])
+plt.plot(g,poweg(g,poptgainint[0],poptgainint[1]),color='blue',
+        label='normalization = %f'%poptgainint[0]+r"$\pm$ "+"%f, index = %f"%(np.sqrt(pcovgainint[0][0]),poptgainint[1]) +r"$\pm$ "+"%f"%np.sqrt(pcovgainint[1][1]))
+plt.errorbar(gains,m,yerr=k_err,ls='None',marker='o')
+plt.xlabel("gain")
+plt.ylabel("intercept [mV]")
+plt.legend()
+
+
+def calibration_params(gain, slopes, slope_er, intercept, intercept_er):
+    err_slope = np.sqrt(powegdi(gain,slopes[0],slopes[1])**2*slope_er[0][0] +
+                        powegdn(gain,slopes[0],slopes[1])**2*slope_er[1][1] +
+                        2*powegdn(gain,slopes[0],slopes[1])*powegdi(gain,slopes[0],slopes[1])*slope_er[1][0])
+    err_intercept = np.sqrt( powegdi(gain,intercept[0], intercept[1])**2*intercept_er[0][0]+
+                             powegdn(gain,intercept[0], intercept[1])**2*intercept_er[1][1]+
+                             2*powegdn(gain,intercept[0],intercept[1])*powegdi(gain,intercept[0],intercept[1])*intercept_er[1][0])
+    return (poweg(gain,slopes[0],slopes[1]),poweg(gain,intercept[0],intercept[1]) ),(err_slope,err_intercept)
+
 #Saving the calibraton in a pickle file
 import pickle
 calibration = dict()
 calibration['gain20'] = popt20
 calibration['gain50'] = popt50
 calibration['gain100'] = popt100
+
+print("Parameters for gain 5 ",calibration_params(5,poptgain3, pcovgain3,poptgainint, pcovgainint))
+print("Parameters for gain 200 ",calibration_params(200,poptgain3, pcovgain3,poptgainint, pcovgainint))
+print("Parameters for gain 500 ",calibration_params(500,poptgain3, pcovgain3,poptgainint, pcovgainint))
+
+def chantoV(chan,param,er_param):
+    err = np.sqrt(chan**2*er_param[0]**2+er_param[1]**2)
+    return (lin_fun(chan,param[0],param[1]),err)
+
+chan = np.linspace(0,512,100)
+plt.figure()
+for g in [5,20,100,200,500]:
+    p,p_err = calibration_params(g,poptgain3, pcovgain3,poptgainint, pcovgainint)
+    f,err =chantoV(chan,p,p_err)
+    plt.errorbar(chan,f,err,label="Gain %d"%g)
+plt.legend()
+
+plt.figure()
+perr =  [np.sqrt(pcov20)[0][0],np.sqrt(pcov20)[1][1]]
+f,err =chantoV(chan,calibration['gain20'],perr)
+plt.errorbar(chan,f,err,label="Gain 20")
+print(pcov20)
+perr =  [np.sqrt(pcov50)[0][0],np.sqrt(pcov50)[1][1]]
+f,err =chantoV(chan,calibration['gain50'],perr)
+plt.errorbar(chan,f,err,label="Gain 50")
+
+
+perr =  [np.sqrt(pcov100)[0][0],np.sqrt(pcov100)[1][1]]
+f,err =chantoV(chan,calibration['gain100'],perr)
+plt.errorbar(chan,f,err,label="Gain 100")
+
+plt.legend()
 
 f = open("../data/calibration.pkl",'w')
 pickle.dump(calibration,f)
